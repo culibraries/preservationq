6048e70dfa80b65a1b368a3a1c223fc896af49e5
Mark Stacy <mbstacy@gmail.com>
2018-03-22 15:28:25 -0600

    added dependences

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='preservationq',
-      version='0.0',
+      version='0.0.1',
       packages= find_packages(),
+      install_requires=[
+          'celery==3.1.22',
+          'pymongo==3.2.1',
+          'requests==2.9.1',
+          'bagit',
+      ],
 )
