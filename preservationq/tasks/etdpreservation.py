f46f68e6073bf686e465db2188da0d161ddcb1e3
Mark Stacy <mbstacy@gmail.com>
2018-03-23 15:40:38 -0600

    change mv files as entire directory

--- preservationq/tasks/etdpreservation.py
+++ preservationq/tasks/etdpreservation.py
@@ -96,16 +96,16 @@ def runExtractRename(pattern):
         ID = getID(xml)
         newpath = sdate + '_' + author.replace(' ', '') + '_' + ID
         created_dirs.append(ETDTGT + newpath)
-        
-        for etd in os.listdir(td):
-            try:
-                os.mkdir(ETDTGT + newpath)
-                # Move ETD files from temp to target directory
-                shutil.move(td + etd, ETDTGT + newpath)
-                # Log the transacton
-                log(os.path.basename(f))
-            except Exception as inst:
-                log("{0}: {1}\n".format(os.path.basename(f),inst))
+
+        #for etd in os.listdir(td):
+        try:
+            os.mkdir(ETDTGT + newpath)
+            # Move ETD files from temp to target directory
+            shutil.move(td, ETDTGT + newpath)
+            # Log the transacton
+            log(os.path.basename(f))
+        except Exception as inst:
+            log("{0}: {1}\n".format(os.path.basename(f),inst))
         # Cleanup the temp directory
         shutil.rmtree(td)
     return created_dirs
